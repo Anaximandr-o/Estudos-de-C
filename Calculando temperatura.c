b05268716d7f10f0dd4c83cecefd80a17e1af31b
Anaximandr-o <116591655+Anaximandr-o@users.noreply.github.com>
2023-04-11 16:55:46 -0300

    Update and rename Exercícios do Fabrício Parte 2.c to Calculando temperatura.c

--- Calculando temperatura.c
+++ Calculando temperatura.c
@@ -3,19 +3,13 @@
 int main (void)
 {
 
-
-
+    //como calcular temperatura :o
+  
     int Celsius;
     printf ("Digite uma temperatura em Celsius: ");
     scanf ("%d", &Celsius);
     Celsius = (9*Celsius+160)/5;
     printf("A mesma temperatura em Fahrenheit =  %d", Celsius);
     return 0;
-
-
-
-
-
-
 
 }
